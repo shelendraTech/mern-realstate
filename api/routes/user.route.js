a01fe3272aeb921797a644d20984d2bf447fafad
Shelendra <singhshelendra70@gmail.com>
2024-05-20 10:29:45 +0530

    react redux

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,11 +2,4 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get("/test", (req,res)=>{
-    res.json({
-        message:"Hello world"
-    })
-})
-
- 
 export default router;
--- clients/src/main.jsx
+++ clients/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'; // Ensure this imports your Tailwind CSS file
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // Ensure this imports your Tailwind CSS file
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
